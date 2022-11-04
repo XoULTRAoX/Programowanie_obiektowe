ea89c870617937d539d9adb8bf3c53ab5105c4fb
XoULTRAoX <115872108+XoULTRAoX@users.noreply.github.com>
2022-11-04 23:22:15 +0100

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,17 +1,17 @@
-public class Iteam {
+public class Item {
 
    String name;
    int quantity;
 
-   public Iteam(String name, int quantity) {
+   public Item(String name, int quantity) {
        this.name = name;
        this.quantity = quantity;
    }
 
    public static void main(String[] args) {
-       Iteam s_axe = new Iteam("Steel Axe", 14);
-       Iteam ci_axe = new Iteam("Cast Iron Axe", 3);
-       Iteam s_saw = new Iteam("Steel Saw", 21);
+       Iteam s_axe = new Item("Steel Axe", 14);
+       Iteam ci_axe = new Item("Cast Iron Axe", 3);
+       Iteam s_saw = new Item("Steel Saw", 21);
        System.out.println("Shop equipment");
        System.out.println("Name: "+s_axe.name+ " - Quantity: "+s_axe.quantity);
        System.out.println("Name: "+ci_axe.name+ " - Quantity: "+ci_axe.quantity);
