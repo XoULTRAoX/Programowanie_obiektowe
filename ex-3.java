420b48c20963d6c38f09e98306dac85b757d2755
XoULTRAoX <115872108+XoULTRAoX@users.noreply.github.com>
2022-11-05 13:40:18 +0100

    Update ex-3.java

--- ex-3.java
+++ ex-3.java
@@ -1,17 +1,11 @@
 public class Theater
 {
 	public static void main(String[] args) {
-		
-		
 		Actors actors1 = new Actors("Anna Er",25,2.1);
-		
 		Movie movie1=new Movie("Marvel","comedy",120,actors1);
 		System.out.println(movie1.movieInformation());
 	}
 }
-
-
-
 
 class Movie
 {
@@ -19,14 +13,12 @@ class Movie
     private String category;
     private int lenght;
     private Actors movieActors;
-    
     Movie(String nm,String cg,int le,Actors ac)
     {
         this.name=nm;
         this.category=cg;
         this.lenght=le;
         this.movieActors=ac;
-        
     }
     
     public String movieInformation()
@@ -34,38 +26,22 @@ class Movie
         String movies;
         movies=movieActors.engineInformation();
         String data=String.format("Movie name: %s \rCategory: %s \rLenght:  %d min \r%s\n",this.name,this.category,this.lenght,movies);
-        
-        
         return data;
-        
-    }
-    
-
-    
-    
+    } 
 }
 class Actors{
     private String name;
     private int age;
     private double rate;
-    
-    
     Actors(String nm, int ag, double rt)
     {
         this.age=ag;
         this.name=nm;
         this.rate=rt;
-        
     }
-    
     public String engineInformation()
     {   
         String data=String.format("Actor name: %s \rAge: %d \rHis rate on Filmweb: %.1f",this.name,this.age,this.rate);
-        
         return data;
-        
-        
-    }
-    
-    
+    }   
 }
